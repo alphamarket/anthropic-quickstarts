0dfd8592260ca28ac4581f446b0c2b26ca96c4e2
Dariush Hasanpour <b.g.dariush@gmail.com>
2025-01-29 20:47:31 +0330

    + image/envify

--- computer-use-demo/image/entrypoint.sh
+++ computer-use-demo/image/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
+./envify index.html > /tmp/index.html
+mv /tmp/index.html index.html
+
 ./start_all.sh
 ./novnc_startup.sh
 
 python http_server.py > /tmp/server_logs.txt 2>&1 &
 
 STREAMLIT_SERVER_PORT=8501 python -m streamlit run computer_use_demo/streamlit.py > /tmp/streamlit_stdout.log &
+
 
 echo "✨ Computer Use Demo is ready!"
 echo "➡️  Open http://localhost:8080 in your browser to begin"
